95b342455741eeb1d85dee1b0ac6e66604732e65
Nemanja Nikolic <nemanja123444@gmail.com>
2021-05-30 20:23:49 +0200

    Implemented work calendar for nurses

--- src/main/java/com/ftn/domzdravlja/controller/TerminController.java
+++ src/main/java/com/ftn/domzdravlja/controller/TerminController.java
@@ -2,20 +2,28 @@ package com.ftn.domzdravlja.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ftn.domzdravlja.dto.PacijentDTO;
 import com.ftn.domzdravlja.dto.TerminDTO;
+import com.ftn.domzdravlja.model.Pacijent;
 import com.ftn.domzdravlja.model.Termin;
 import com.ftn.domzdravlja.service.KlinikaService;
 import com.ftn.domzdravlja.service.OsobljeService;
 import com.ftn.domzdravlja.service.TerminService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 @RequestMapping("/termin")
 public class TerminController {
@@ -41,5 +49,25 @@ public class TerminController {
 		
 		return new ResponseEntity<TerminDTO>(new TerminDTO(save), HttpStatus.OK);
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<TerminDTO>> findAll() {
+		List<Termin> termini = terminService.findAll();
+		
+		List<TerminDTO> dtoList = new ArrayList<TerminDTO>();
+		
+		for (Termin termin : termini) {
+			dtoList.add(new TerminDTO(termin));
+		}
+		
+		return new ResponseEntity<>(dtoList, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<TerminDTO> get(@PathVariable("id") Integer id) {
+		Termin termin = terminService.findTerminById(id);
+		return new ResponseEntity<TerminDTO>(new TerminDTO(termin), HttpStatus.OK);
+	}
+
 
 }
--- src/main/java/com/ftn/domzdravlja/service/TerminService.java
+++ src/main/java/com/ftn/domzdravlja/service/TerminService.java
@@ -1,8 +1,14 @@
 package com.ftn.domzdravlja.service;
 
+import java.util.List;
+
 import com.ftn.domzdravlja.model.Termin;
 
 public interface TerminService {
 
 	Termin saveTermin(Termin termin);
+	
+	List<Termin> findAll();
+	
+	Termin findTerminById(Integer id);
 }
